06e8e10dd5b7ca2cbd0e2598551f038002cd57fe
Tam Lu <tamlu907@gmail.com>
2016-04-05 12:55:38 -0700

    CS16201 added

---
+++ 162/01/run_facts.cpp
@@ -0,0 +1,13 @@
+#include "state_facts.h"
+
+int main(int argc, int * argv[]) {
+    int state_size; string file_name;
+    if (argc == 5) {
+        struct state * county_array = new state[];
+        struct county * county_array = new county[];
+    }
+    else {
+        cout << "Error: Invalid amount of arguments.\n";
+    }
+	return 0;
+}
---
+++ 162/01/state_facts.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+struct county {
+    string name;
+    string *city;
+    int cities;
+    int population;
+    float avg_income;
+    float avg_house;
+};
+struct state {
+    string name;
+    struct county *c;
+    int counties;
+    int population;
+};
+
+bool is_valid_arguments(char *info[]);
+state * create_states(int);
+void get_state_data(state *);
+county * create_counties(int);
+void get_county_data(county *);
+void delete_info(state *);
+void largest_population(int);
+void sorted_name(int);
+void sorted_population(int);
+int main(int, int**);
